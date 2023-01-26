451d13f50aa826df3ee62f4f05cfcc20051921f3
Vera Weikel <vera.weikel@gmail.com>
2023-01-26 11:04:01 -0800

    WIP: Writes to GameObj.score; It will add if/else on js to proto.playerTurn() test2.
    Co-authored by: Erin Timlin <erintimlin@gmail.com>
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,10 @@ function GameBoard() {
 function Player(player1, player2) { 
   this.player1 = player1; 
   this.player2 = player2;
+  this.player1Score = this.player1Score;
+  this.player2Score = this.player2Score;
 }
-// players need an id (similar to addBk.)
 
-// this function starts a round score
-
-// to experiment with -- start here in the morning
 let diceRoll = function() {
   let currentScore = 0;
   let min = Math.ceil(1); //declare min here
@@ -22,38 +20,25 @@ let diceRoll = function() {
   currentScore += roll;
   return roll; 
 }
-// let gameBoard = new GameBoard
-// diceRoll(gameBoard);
-// seems to be working
+let gameBoard = new GameBoard; // fake db
 
-// // this is what works -- this is our original
-// GameBoard.prototype.score = function() {
-//   let currentScore = 0;
-//   let min = Math.ceil(1); //declare min here
-//   let max = Math.floor(7); //declare max here; max must be 7 because max < Math.floor()
-//   let roll =  Math.floor(Math.random() * (max - min) + min);
-//   currentScore += roll;
-//   return roll; 
-// }
+// dxn: change the return; event handler for click; if/else: 
 
 GameBoard.prototype.playerTurn = function() {
-  // player if/else turns
-  // try adding diceRoll in here
   let updatedScore = diceRoll();
-  console.log(updatedScore)
-  if (this.updatedScore === 1) {
-    updatedScore = 0;
-  } else if (this.updatedScore !== 1) {
-    updatedScore += diceRoll();
-    console.log(diceRoll);
+  let currentTurnScore = updatedScore;
+  console.log("updatedScore, currentTurnScore: ", updatedScore, currentTurnScore);
+  
+  if (this.currentTurnScore === 1) {
+    // this.roundScore = 0;
+    console.log("if rs: ", this.roundScore);
+    return this.roundScore;
   } 
-
-  return updatedScore;
-  // let something = the value we pull in for diceRoll();
-  // if that something === 1 then score will be 0
-  // if that something !== 1 then roll again and add that to something
-
-
+  else if (this.currentTurnScore !== 1) {
+    this.roundScore += currentTurnScore; // current turn tally until a 1 is rolled;
+    console.log("else t.rs: ", this.roundScore);
+  } 
+  return this.roundScore;
 }
 
 
@@ -65,12 +50,12 @@ GameBoard.prototype.playerTurn = function() {
 // GameBoard.protoype.currentTurn()
 //players.player[id] = this.turn; //pseudocode
 
-//Die roll function
-function getRandomInt(min, max) {
-  min = Math.ceil(1); //declare min here
-  max = Math.floor(7); //declare max here
-  return Math.floor(Math.random() * (max - min) + min);
-}
-console.log(getRandomInt()); //call it without number inside parens b/c already defined
+// //Die roll function
+// function getRandomInt(min, max) {
+//   min = Math.ceil(1); //declare min here
+//   max = Math.floor(7); //declare max here
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+// console.log(getRandomInt()); //call it without number inside parens b/c already defined
 
 //getting a round score: start originally with 0, add in getRandomInt until player rolls a 1 or holds.
